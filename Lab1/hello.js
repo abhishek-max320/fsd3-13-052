e33fef1df2e0050bc4b04063ed67f179422a92ba
Abhishek Sonkar <abhishek.25b01010800@abes.ac.in>
2026-07-21 14:48:10 +0530

    add function

---
+++ Lab1/hello.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+    console.log(`welcome to $(name)`);
+}
+
+function sum(a, b) {
+    return a + b;
+
+}
+
+sayHello("abhishek sonkar");
+console.log(`sum of 2 and 5 is ${sum(2, 5)}`);
