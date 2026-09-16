e7da5d09e365c34de0dca510a59aad9ddba0acaf
Abhishek Sonkar <abhishek.25b01010800@abes.ac.in>
2026-09-16 12:20:28 +0530

    put npm

--- Lab3/prg6.js
+++ Lab3/prg6.js
@@ -8,8 +8,19 @@ const server = http.createServer((req, res) => {
     }
 
     else if (req.url === "/" && req.method === "POST") {
-        res.statusCode = 200;
-        res.end("POST Request");
+        //console.log("Request:", req);
+        let body = ' '
+        req.on('data', (chunk) => {
+            body += chunk
+
+        })
+        req.on("end", () => {
+            const product = JSON.parse(body);
+            console.log("received products:", product);
+            res.statusCode = 201;
+        res.end(JSON.stringify({msg:'product added',product}));
+        });
+        
     }
 
     else if (req.url === "/" && req.method === "PUT") {
